1de0cc4b05a468a77cce273ff8bfa8ef1e1717c9
ogabek <norpulatovogabek42@gmail.com>
2022-08-01 10:40:41 +0500

    task1

--- bin/basic_class_homework02.dart
+++ bin/basic_class_homework02.dart
@@ -2,5 +2,10 @@
 Create a "Person" class. 
 Create an attribute "name" in the "Person" class.
 */
+class Person {
+  Person(name) {}
+}
 
-void main() {}
+void main() {
+  print(Person('ogabek'));
+}

e6f83e83b2352124578b4340a57f7b2129fa56ed
ogabek <norpulatovogabek42@gmail.com>
2022-08-01 11:23:10 +0500

    task5

--- bin/basic_class_homework05.dart
+++ bin/basic_class_homework05.dart
@@ -8,5 +8,18 @@ Create an object named "p2" whose name is "Shavkat"
 Create an object named "p3" whose name is "Jasur"
 
 */
+class Person {
+  String name = '';
+  Person(name) {
+    this.name = name;
+  }
+}
 
-void main() {}
+void main() {
+  Person p1 = Person('Anvar');
+  Person p2 = Person('Shavkat');
+  Person p3 = Person('Jasur');
+  print(p1.name);
+  print(p2.name);
+  print(p3.name);
+}
